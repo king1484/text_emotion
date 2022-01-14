a93acdac0ed040db38e1ab78ba78d701161843f3
king1484 <85832194+king1484@users.noreply.github.com>
2022-01-14 18:42:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from textblob import TextBlob
 
 app = Flask(__name__)
 
 @app.route("/")
 def get():
-    return "Hello world"
+    return str(TextBlob("Nice").polarity)
+    
+
 if __name__ == "__main__":
     app.run()
