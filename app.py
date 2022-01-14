7d56bc400f1e6e03a05084f64e2589d00e3fd2a7
king1484 <85832194+king1484@users.noreply.github.com>
2022-01-14 18:44:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def get():
-    return str(TextBlob("Nice").polarity)
+    return str(TextBlob("Hey its working").polarity)
     
 
 if __name__ == "__main__":
